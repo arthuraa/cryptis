7d52d304ad1a21cc0b1b124bb9a24f3a57a124f8
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-08-25 13:33:17 -0400

    Extract term definition to a separate file.

--- crypto.v
+++ crypto.v
@@ -8,76 +8,13 @@ From iris.heap_lang Require Import notation proofmode metatheory.
 From iris.heap_lang Require Import primitive_laws.
 From iris.base_logic.lib Require Import invariants.
 From iris_string_ident Require Import ltac2_string_ident.
-From crypto Require Import lib basic symbols.
+From crypto Require Import lib basic symbols term.
 
 Definition termN := nroot.@"term".
 
 Section Terms.
 
 Context `{heapG Σ, cfgSG Σ, symbolSG Σ}.
-
-Inductive term  :=
-| TInt of Z
-| TPair of term & term
-| TNonce of loc
-| TKey of loc
-| TEnc of loc & term.
-
-Canonical termO := leibnizO term.
-
-Global Instance term_eq_dec : EqDecision term.
-Proof.
-refine (
-  fix go t1 t2 {struct t1} : Decision (t1 = t2) :=
-    match t1, t2 with
-    | TInt n1, TInt n2 =>
-      cast_if (decide (n1 = n2))
-    | TPair t11 t12, TPair t21 t22  =>
-      cast_if_and (decide (t11 = t21)) (decide (t12 = t22))
-    | TNonce l1, TNonce l2 =>
-      cast_if (decide (l1 = l2))
-    | TKey l1, TKey l2 =>
-      cast_if (decide (l1 = l2))
-    | TEnc l1 t1, TEnc l2 t2 =>
-      cast_if_and (decide (l1 = l2)) (decide (t1 = t2))
-    | _, _ => right _
-    end); clear go; abstract intuition congruence.
-Defined.
-
-Fixpoint val_of_term t : val :=
-  match t with
-  | TInt n => (#0, #n)
-  | TPair t1 t2 => (#1, (val_of_term t1, val_of_term t2))%V
-  | TNonce l => (#2, #l)%V
-  | TKey l => (#3, #l)%V
-  | TEnc l t => (#4, (#l, val_of_term t))
-  end.
-
-Fixpoint term_of_val v : term :=
-  match v with
-  | PairV (# (LitInt 0)) (# (LitInt n)) =>
-    TInt n
-  | PairV (# (LitInt 1)) (PairV v1 v2) =>
-    TPair (term_of_val v1) (term_of_val v2)
-  | PairV (# (LitInt 2)) (# (LitLoc l)) =>
-    TNonce l
-  | PairV (# (LitInt 3)) (# (LitLoc l)) =>
-    TKey l
-  | PairV (# (LitInt 4)) (PairV (# (LitLoc l)) v) =>
-    TEnc l (term_of_val v)
-  | _ => TInt 0
-  end.
-
-Global Instance val_of_termK : Cancel (=) term_of_val val_of_term.
-Proof. elim=> //= *; congruence. Qed.
-
-Global Instance val_of_term_inj : Inj (=) (=) val_of_term.
-Proof.
-by apply (@cancel_inj _ _ _ term_of_val); apply _.
-Qed.
-
-Global Instance countable_term : Countable term.
-Proof. apply (inj_countable' _ _ val_of_termK). Qed.
 
 Class termSG := TermSG {
   term_inG :> inG Σ (authR (gsetUR (matchingO termO)));
@@ -557,6 +494,8 @@ Definition lo_term v1 v2 : iProp Σ :=
 
 Global Instance persistent_lo_term v1 v2 : Persistent (lo_term v1 v2).
 Proof. apply _. Qed.
+
+Definition mknonce : val := (λ: <>, ref #()).
 
 Definition eq_term : val := (rec: "eq" "x" "y" :=
   if: (Fst "x" = #0) && (Fst "y" = #0) then
