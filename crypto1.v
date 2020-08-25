daeb0ea96760fcfbda1c5aa8afc96b4d4d5b4896
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2020-08-25 16:16:13 -0400

    Progress on well-formedness condition for terms.

--- crypto1.v
+++ crypto1.v
@@ -1,6 +1,6 @@
 From mathcomp Require Import ssreflect.
 From stdpp Require Import gmap.
-From iris.algebra Require Import agree.
+From iris.algebra Require Import agree auth gmap.
 From iris.heap_lang Require Import notation proofmode.
 From crypto Require Import term.
 
@@ -10,14 +10,33 @@ Inductive readers :=
 
 Canonical readersO := leibnizO readers.
 
-Instance readers_valid : Valid readers := λ _, True.
-Instance readers_validN : ValidN readers := λ _ _, True.
-Instance readers_pcore : PCore readers := λ x, Some x.
-Instance readers_op : Op readers := λ rs1 rs2,
+Instance readers_elem_of : ElemOf loc readers := λ l rs,
+  match rs with
+  | RPub => True
+  | RPriv rs => l ∈ rs
+  end.
+Instance readers_subseteq : SubsetEq readers := _.
+Instance readers_singleton : Singleton loc readers := λ l,
+  RPriv {[l]}.
+Instance readers_union : Union readers := λ rs1 rs2,
   match rs1, rs2 with
   | RPriv rs1, RPriv rs2 => RPriv (rs1 ∪ rs2)
   | _, _ => RPub
   end.
+Instance readers_empty : Empty readers := RPriv ∅.
+Instance readers_semiset : SemiSet loc readers.
+Proof.
+split.
+- by move=> x; rewrite /elem_of /=; apply/elem_of_empty; case.
+- by move=> x y; rewrite /elem_of /= elem_of_singleton.
+- move=> [|X] [|Y] l; rewrite /union /elem_of /=; try by intuition eauto.
+  by rewrite elem_of_union.
+Qed.
+
+Instance readers_valid : Valid readers := λ _, True.
+Instance readers_validN : ValidN readers := λ _ _, True.
+Instance readers_pcore : PCore readers := λ x, Some x.
+Instance readers_op : Op readers := union.
 
 Lemma readers_cmra_mixin : CmraMixin readers.
 Proof.
@@ -131,5 +150,37 @@ split.
     by exists (RKey P21'), (RKey P22'); intuition eauto.
 Qed.
 Canonical resR := CmraT res res_cmra_mixin.
+
+Definition resM := gmapUR loc resR.
+Implicit Types RM : resM.
+
+Class resG := {
+  res_inG :> inG Σ (authR resM);
+  res_name : gname
+}.
+
+Context `{!resG}.
+
+Definition wf_readers rs RM :=
+  match rs with
+  | RPub     => True
+  | RPriv rs => ∀l, l ∈ rs → ∃ P, RM !! l = Some (RKey P)
+  end.
+
+Definition wf_resM RM :=
+  ∀ l rs, RM !! l = Some (RNonce rs) → wf_readers rs RM.
+
+(** [term_readers t rs] holds when the term [t] can be declared public after
+encrypting it with the readers rs.  If [rs = RPub], [t] does not have to be
+encrypted. *)
+
+Fixpoint term_readers t rs : iProp Σ :=
+  match t with
+  | TInt _ => True
+  | TPair t1 t2 => term_readers t1 rs ∗ term_readers t2 rs
+  | TNonce l => ∃ rs', own res_name (◯ {[l := RNonce rs']}) ∗ ⌜rs ⊆ rs'⌝
+  | TKey l => True (* Wrong for now; keys should also be private *)
+  | TEnc l t => term_readers t {[l]}
+  end.
 
 End Resources.
