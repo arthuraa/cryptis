a4b1a54f37060123bfa0752459431aaab6c9f5dc
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2022-07-26 10:27:42 -0400

    Update Coq version.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 
-let coq = coq_8_14; in
-let coqPackages = coqPackages_8_14; in
+let coq = coq_8_15; in
+let coqPackages = coqPackages_8_15; in
 let ssreflect = coqPackages.mathcomp.ssreflect; in
 let deriving = coqPackages.deriving; in
 let stdpp = coqPackages.stdpp; in
