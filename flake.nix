f7a31aff3fb58406910e355b9793a3671a980330
Dongyu Wu <wdy9611@gmail.com>
2025-09-08 22:30:04 -0400

    Merge Add Actris flake.

--- flake.nix
+++ flake.nix
@@ -3,56 +3,54 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-utils.url = "github:numtide/flake-utils";
+    actris.url = "git+https://gitlab.mpi-sws.org/iris/actris.git";
+    actris.flake = false;
+    stdpp-1-12.url = "git+https://gitlab.mpi-sws.org/iris/stdpp.git?ref=refs/tags/coq-stdpp-1.12.0";
+    stdpp-1-12.flake = false;
   };
 
-  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+  # outputs = { self, nixpkgs, stdpp, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, actris, stdpp-1-12 }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs =
+          import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        lib = pkgs.lib;
+      in
+        {
+          devShell = pkgs.mkShell {
+            packages = [
+              pkgs.coq
+              pkgs.coqPackages.mathcomp.ssreflect
+              pkgs.coqPackages.deriving
+              pkgs.coqPackages.iris
+              pkgs.coqPackages.actris
+            ];
+          };
+        }
+    ) //
+    {
+      overlays.default = final: prev: {
+        coqPackages = prev.coqPackages.overrideScope (final: prev: {
+          actris = prev.mkCoqDerivation {
+            pname = "actris";
+            defaultVersion = "dev";
+            release.dev.src = actris;
+            propagatedBuildInputs = [
+              final.coq
+              final.iris
+            ];
+          };
 
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        };
-
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.coqPackages.cryptis;
-      };
-      flake = {
-
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-        overlays.default = final: prev: {
-          coqPackages = prev.coqPackages.overrideScope (final: prev: {
-            cryptis = prev.mkCoqDerivation {
-              pname = "cryptis";
-              defaultVersion = "dev";
-              release.dev.src = ./.;
-              useDune = true;
-              propagatedBuildInputs = [
-                final.coq
-                final.mathcomp.ssreflect
-                final.deriving
-                final.iris
-                final.stdlib
-              ];
-            };
-          });
-        };
-
+          stdpp = prev.lib.overrideCoqDerivation {
+            defaultVersion = "1.12";
+            release."1.12".src = stdpp-1-12;
+          } prev.stdpp;
+        });
       };
     };
 }
