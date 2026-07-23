3d8ce3f991aecd3ca20199543cfdeb632101e5da
Dongyu Wu <winterdawnyu@gmail.com>
2026-07-23 14:35:28 -0400

    finish send tag proofs

--- examples/sess/sketch_send_tag.v
+++ examples/sess/sketch_send_tag.v
@@ -30,7 +30,23 @@ Lemma sess_send_msg skI skR si rl (m : iMsg Σ term) t p ts_send ts_recv :
     GenConn.chan_inv_for sess_ctx skI skR si rl (ts_send ++ [t]) ts_recv ∗
     sess_own skI skR si rl p.
 Proof.
-Admitted.
+iIntros "(%γs & #Hγs & own) Hm (%γs' & >#Hγs' & ctx)".
+iPoseProof (session_names_agree with "Hγs Hγs'") as "<-".
+iClear "Hγs'". case: rl => /=.
+- iApply fupd_mask_intro; first set_solver.
+  iIntros "close !> !>".
+  iMod (iProto_send with "ctx own Hm") as "[ctx own]".
+  iApply step_fupdN_intro => //.
+  iIntros "!> !>". iMod "close" as "_".
+  iModIntro. by iFrame; eauto.
+- rewrite iProto_ctx_sym.
+  iApply fupd_mask_intro; first set_solver.
+  iIntros "close !> !>".
+  iMod (iProto_send with "ctx own Hm") as "[ctx own]".
+  iApply step_fupdN_intro => //.
+  iIntros "!> !>". iMod "close" as "_".
+  iModIntro. rewrite iProto_ctx_sym. by iFrame; eauto.
+Qed.
 
 Lemma wp_send_msg skI skR rl cs (m : iMsg Σ term) t p :
   {{{ connected skI skR rl cs (<!> m) ∗
@@ -39,13 +55,23 @@ Lemma wp_send_msg skI skR rl cs (m : iMsg Σ term) t p :
     impl.send (repr cs) t
   {{{ RET #(); connected skI skR rl cs p }}}.
 Proof.
-Admitted.
+iIntros (Φ) "((c & own) & #p_t & Hdisj) post". wp_lam; wp_pures.
+wp_apply (GenConn.wp_send_fupdN (λ skI skR si, sess_own skI skR si rl p)
+           with " [//] [$c own Hdisj]").
+{ iDestruct "own" as "[#fail|own]"; eauto.
+  iDestruct "Hdisj" as "[#fail|Hm]"; eauto.
+  iRight. iIntros (ts_send ts_recv) "inv".
+  iMod (sess_send_msg with "own Hm inv") as "upd". by iIntros "!>". }
+iIntros "[??]"; iApply "post". by iFrame.
+Qed.
 
 Lemma iMsg_tag_intro (ms : gmap namespace (iMsg Σ term)) N m t' pp :
   ms !! N = Some m →
   iMsg_car m t' pp -∗ iMsg_car (iMsg_tag ms) (Spec.tag (Tag N) t') pp.
 Proof.
-Admitted.
+move=> HN. rewrite iMsg_tag_eq. iIntros "H".
+iExists N, t', m. iFrame. by iPureIntro.
+Qed.
 
 Lemma wp_send_tag {TT : tele} skI skR rl cs
     (ms : gmap namespace (iMsg Σ term)) N
@@ -57,34 +83,75 @@ Lemma wp_send_tag {TT : tele} skI skR rl cs
     impl.send (repr cs) (Spec.tag (Tag N) (t x))
   {{{ RET #(); connected skI skR rl cs (p x) }}}.
 Proof.
-Admitted.
+move=> HN. iIntros (Φ) "(conn & #p_t & Hdisj) post".
+iApply (wp_send_msg _ _ _ _ (iMsg_tag ms) _ (p x) with "[conn Hdisj] post").
+iSplitL "conn"; first by rewrite /iProto_tag.
+iSplitR; first by rewrite public_tag.
+iDestruct "Hdisj" as "[#fail|HP]"; first by iLeft.
+iRight. iApply (iMsg_tag_intro _ _ HN).
+rewrite iMsg_texist_exist bi_texist_exist. iExists x.
+rewrite iMsg_base_eq /=.
+iSplit; first done.
+iFrame "HP".
+auto.
+Qed.
+
+Lemma iMsg_map_tag (f : iProto Σ term → iProto Σ term)
+    (ms : gmap namespace (iMsg Σ term)) :
+  iMsg_map f (iMsg_tag ms) ≡ iMsg_tag ((λ m, iMsg_map f m) <$> ms).
+Proof.
+rewrite iMsg_tag_unseal. intros v lp; simpl. iSplit.
+- iDestruct 1 as (p1) "[H Heq]".
+  iDestruct "H" as (N t' m0 [HN ->]) "Hm".
+  iExists N, t', (iMsg_map f m0).
+  iSplit; first by iPureIntro; rewrite lookup_fmap HN.
+  simpl. iExists p1. iFrame.
+- iDestruct 1 as (N t' m0 [HN ->]) "Hm".
+  move: HN; rewrite lookup_fmap.
+  case E: (ms !! N) => [m1|] //= => -[<-].
+  iDestruct "Hm" as (p1) "[Hm Heq]".
+  iExists p1. iFrame "Heq".
+  iExists N, t', m1. iFrame. by iPureIntro.
+Qed.
 
 Lemma iMsg_dual_tag (ms : gmap namespace (iMsg Σ term)) :
   iMsg_dual (iMsg_tag ms) ≡ iMsg_tag (iMsg_dual <$> ms).
-Proof.
-Admitted.
+Proof. apply iMsg_map_tag. Qed.
 
 Lemma iMsg_app_tag (ms : gmap namespace (iMsg Σ term)) (q : iProto Σ term) :
   (iMsg_tag ms <++> q)%msg ≡ iMsg_tag ((λ m, (m <++> q)%msg) <$> ms).
-Proof.
-Admitted.
+Proof. apply iMsg_map_tag. Qed.
 
 Lemma iProto_dual_tag (a : action) (ms : gmap namespace (iMsg Σ term)) :
   iProto_dual (iProto_tag a ms)
   ≡ iProto_tag (action_dual a) (iMsg_dual <$> ms).
-Proof.
-Admitted.
+Proof. by rewrite /iProto_tag iProto_dual_message iMsg_dual_tag. Qed.
 
 Lemma iProto_app_tag (a : action) (ms : gmap namespace (iMsg Σ term))
     (q : iProto Σ term) :
   (iProto_tag a ms <++> q)%proto
   ≡ iProto_tag a ((λ m, (m <++> q)%msg) <$> ms).
-Proof.
-Admitted.
+Proof. by rewrite /iProto_tag iProto_app_message iMsg_app_tag. Qed.
 
 Lemma iMsg_tag_proper :
   Proper ((≡) ==> (≡)) (iMsg_tag (Σ:=Σ)).
 Proof.
-Admitted.
+rewrite iMsg_tag_unseal.
+intros ms1 ms2 Hms v lp; simpl. iSplit.
+- iDestruct 1 as (N t' m0 [HN ->]) "Hm".
+  move: (Hms N); rewrite HN.
+  case E: (ms2 !! N) => [m2|] Hopt; last by inversion Hopt.
+  inversion Hopt as [?? He|]; subst.
+  iExists N, t', m2.
+  iSplit; first by iPureIntro.
+  iApply (bi.equiv_entails_1_1 _ _ (He t' lp)). iExact "Hm".
+- iDestruct 1 as (N t' m0 [HN ->]) "Hm".
+  move: (Hms N); rewrite HN.
+  case E: (ms1 !! N) => [m2|] Hopt; last by inversion Hopt.
+  inversion Hopt as [?? He|]; subst.
+  iExists N, t', m2.
+  iSplit; first by iPureIntro.
+  iApply (bi.equiv_entails_1_2 _ _ (He t' lp)). iExact "Hm".
+Qed.
 
 End SketchSendTag.
